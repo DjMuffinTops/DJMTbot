94018e212c2b86d8eca366e7ed3192bafa412439
djmuffintops <djmuffintops@gmail.com>
2020-07-26 14:24:57 -0400

    Update day of the week images

--- src/jobs/dotwConstants.ts
+++ src/jobs/dotwConstants.ts
@@ -1,19 +1,19 @@
 export const dayOfTheWeek: any = {
     "0": {
         day: "Sunday",
-        messages:["https://cdn.discordapp.com/attachments/688848577212317744/736226163873021972/video0.mp4", "https://cdn.discordapp.com/attachments/688848577212317744/735730083125526529/image0-42.jpg"]
+        messages:["https://cdn.discordapp.com/attachments/718331550953111602/737012518333186109/390aaab73e969fc9a912c447e51011b2850f84cdv2_hq.png"]
     },
     "1": {
         day: "Monday",
-        messages:[]
+        messages:["https://cdn.discordapp.com/attachments/718331550953111602/737008126678794331/riseandshine.mp4"]
     },
     "2": {
         day: "Tuesday",
-        messages:[]
+        messages:["https://media.discordapp.net/attachments/688848577212317744/732624561132011520/image0.jpg"]
     },
     "3": {
         day: "Wednesday",
-        messages:[]
+        messages:["https://cdn.discordapp.com/attachments/718331550953111602/737005861960024074/thsrb5is0xtz.png"]
     },
     "4": {
         day: "Thursday",
@@ -25,6 +25,6 @@ export const dayOfTheWeek: any = {
     },
     "6": {
         day: "Saturday",
-        messages:[]
+        messages:["https://cdn.discordapp.com/attachments/718331550953111602/737009132774752286/saturday.mp4"]
     }
 }
