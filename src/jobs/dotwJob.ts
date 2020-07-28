5998e8a635f2c8152d759ea0966dbe039420bb62
djmuffintops <djmuffintops@gmail.com>
2020-07-28 01:12:45 -0400

    Added extra logs and simplified dotw text

--- src/jobs/dotwJob.ts
+++ src/jobs/dotwJob.ts
@@ -6,16 +6,17 @@ const gRegistry = require("../../json/guild/guildRegistry.json");
 
 export async function dotwJob(client: Client) {
     const date = new Date();
-    console.log(`Running Day of the Week Job: ${date.toLocaleDateString()} ${date.toLocaleTimeString()}` );
+    const today: DayOfTheWeek = dayOfTheWeek[date.getDay()];
+    console.log(`Running Day of the Week Job: ${today.day} ${date.toLocaleDateString()} ${date.toLocaleTimeString()}` );
     for (const guildId of Object.keys(gRegistry)) {
         let guild: GuildRegistry = gRegistry[guildId];
         if (guild && guild.dotwChannels) {
             for (const channelId of guild.dotwChannels) {
                 const channel = (await client.channels.fetch(channelId) as TextChannel);
                 // Determine which dotw post to send
-                const today: DayOfTheWeek = dayOfTheWeek[date.getDay()];
                 let randomMessage = today.messages[Math.floor(today.messages.length * Math.random())];
-                await channel.send(`${today.day} ${date.toLocaleTimeString()} ${randomMessage ? randomMessage : ''}`);
+                await channel.send(`${date.toLocaleTimeString()} ${randomMessage ? randomMessage : ''}`);
+                console.log(`Sent: ${date.toLocaleTimeString()} ${randomMessage ? randomMessage : ''}` );
             }
         }
     }
