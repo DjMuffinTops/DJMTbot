0cfb54a5110397d628b4581b56f6de4e4e907ed7
DjMuffinTops <68816695+DjMuffinTops@users.noreply.github.com>
2022-04-02 14:59:09 -0400

    Update src/Components/AutoThreadComponent.ts

--- src/Components/AutoThreadComponent.ts
+++ src/Components/AutoThreadComponent.ts
@@ -133,7 +133,6 @@ export class AutoThreadComponent extends Component<AutoThreadComponentSave> {
             });
 			} catch (e) {
 			if (e instanceof Error && e.message === 'Unknown Message')
-            console.log(`[${this.djmtGuild.guildId}] checkAutoThread Unknown message error, message was probably already deleted`);
 
     /**
      * Extracts the best name for a thread from a given message.
