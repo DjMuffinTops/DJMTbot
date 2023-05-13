27d8184a587c38959bb2a034435d1a1983425391
djmuffintops <djmuffintops@gmail.com>
2023-05-13 19:43:56 -0400

    Added express endpoint to pass health checks for deployment

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,18 @@
-import {DJMTbot} from "./DJMTbot";
+import { DJMTbot } from "./DJMTbot";
+const express = require('express')
+const app = express()
+const PORT = 3000;
+
+app.get('/', (req: any, res: any) => {
+    const data = {
+        uptime: process.uptime(),
+        message: 'Ok',
+        date: new Date()
+    }
+    res.status(200).send(data);
+});
+
+// Literally only doing this so digital ocean can pass health checks ugh
+app.listen(PORT, console.log("Server has started at port " + PORT));
 
 DJMTbot.getInstance().run().then(r => console.log('Bot has been run'));
